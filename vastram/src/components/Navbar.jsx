349196365f1a1b7674cfe3c7a14b12593b1461c0
Akash Shaw <shawakash2003@gmail.com>
2023-04-22 13:57:39 +0530

    Adding States

--- vastram/src/components/Navbar.jsx
+++ vastram/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { HiOutlineLogout, HiOutlineMinusCircle } from 'react-icons/hi'
 import { RiAddCircleLine, RiDeleteBin6Line } from 'react-icons/ri'
 import { BsFillBagCheckFill } from 'react-icons/bs'
 
-const Navbar = () => {
+const Navbar = ({ addInCart, removeFromCart, cart, subTotal, clearCart }) => {
     const sideCartRef = useRef();
     const toggleCart = () => {
         if (sideCartRef.current.classList.contains('translate-x-full')) {
@@ -57,141 +57,56 @@ const Navbar = () => {
                 <div onClick={toggleCart} className="flex justify-center text-2xl cursor-pointer"><CgCloseO /></div>
                 <h2 className="heading font-semibold md:font-bold text-[#b6464c] text-base md:text-xl">Shopping Cart</h2>
                 <ol className="list-disc list-outside font-normal flex flex-col w-full gap-y-2 text-sm md:text-lg pl-2 text-black">
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                    {Object.keys(cart).length == 0 ? <div className='font-medium text-lg'>Add items to use this</div> :
+                        Object.keys(cart).map((k) => {
+                            return (
+                                <li className="" key={k}>
+                                    <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                                        <div className="product w-2/3 ">{cart[k]?.name}</div>
+                                        <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
+                                            <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
+                                            <div className="count text-slate-800">{cart[k]?.qty}</div>
+                                            <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
+                                        </div>
+                                        <div className="item-total w-1/6 font-serif">
+                                            ₹{cart[k]?.price}
+                                        </div>
+                                    </div>
+                                </li>
+                            );
+                        })}
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                    {Object.keys(cart).length == 0 ? <></> :
+                        <><div className="text-black flex flex-col gap-y-2 my-2">
+                            <hr />
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                            <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                                <div className="product w-2/3 ">Summary --&gt; </div>
+                                <div className="item-info flex w-1/6 font-serif flex  gap-x-2 w-fit">
+                                    <div className="">Items: </div>
+                                    <div className="count text-slate-800">5</div>
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
+                                </div>
+                                <div className="item-total w-1/6 font-serif">
+                                    ₹500.00
+                                </div>
                             </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
                         </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                            <div className="flex justify-around">
+                                <div className="">
+                                    <button className='md:text-lg text-sm text-white font-medium cursor-pointer bg-[#b6464c] rounded-md md:px-4 px-2 py-1 flex items-center gap-x-2'><BsFillBagCheckFill />CheckOut</button>
+                                </div>
+                                <div className="">
+                                    <button className='md:text-lg text-sm text-white font-medium cursor-pointer bg-[#b6464c] rounded-md px-2 md:px-4 py-1 flex items-center gap-x-2'><RiDeleteBin6Line />Clear</button>
+                                </div>
+                            </div>
+                        </>
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <li className="">
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
+                    }
 
-                            <div className="product w-2/3 ">Product Name</div>
-                            <div className="item-info w-1/6 font-serif flex items-center gap-x-2 w-fit">
-                                <div className="incr text-lg text-slate-700"><RiAddCircleLine /></div>
-                                <div className="count text-slate-800">5</div>
-                                <div className="decr text-slate-700"><HiOutlineMinusCircle /></div>
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </li>
-                    <div className="text-black flex flex-col gap-y-2 my-2">
-                        <hr />
-                        <div className="flex text-sm md:gap-x-4 gap-x-2 items-center text-[14.5px] md:text-lg">
-
-                            <div className="product w-2/3 ">Summary --&gt; </div>
-                            <div className="item-info flex w-1/6 font-serif flex  gap-x-2 w-fit">
-                                <div className="">Items: </div>
-                                <div className="count text-slate-800">5</div>
-
-                            </div>
-                            <div className="item-total w-1/6 font-serif">
-                                ₹500.00
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-around">
-                        <div className="">
-                            <button className='md:text-lg text-sm text-white font-medium cursor-pointer bg-[#b6464c] rounded-md md:px-4 px-2 py-1 flex items-center gap-x-2'><BsFillBagCheckFill />CheckOut</button>
-                        </div>
-                        <div className="">
-                            <button className='md:text-lg text-sm text-white font-medium cursor-pointer bg-[#b6464c] rounded-md px-2 md:px-4 py-1 flex items-center gap-x-2'><RiDeleteBin6Line />Clear</button>
-                        </div>
-                    </div>
                 </ol>
 
             </div>
--- vastram/src/pages/_app.js
+++ vastram/src/pages/_app.js
@@ -2,18 +2,80 @@ import Footer from 'bring/components/Footer';
 import Navbar from 'bring/components/Navbar';
 import 'bring/styles/globals.css'
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }) {
+
+  const [cart, setCart] = useState({});
+  const [subTotal, setSubTotal] = useState(0);
+
+  useEffect(() => {
+    try {
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")));
+      } else {
+        setCart({});
+        saveCart({});
+      }
+    } catch (err) {
+      console.error(err);
+      localStorage.clear()
+    }
+  }, [])
+
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", myCart);
+    let subt = 0;
+    for(let item of myCart) {
+      subt += (item.qty)*(item.price);
+    }
+    setSubTotal(subt);
+  }
+
+  const addInCart = (itemCode, qty, price, size, name, color) => {
+    const myCart = cart;
+    if (itemCode in myCart) {
+      myCart[itemCode].qty += qty;
+    } else {
+      myCart[itemCode] = {
+        qty,
+        price,
+        size,
+        name,
+        color
+      };
+    }
+    setCart(myCart);
+    saveCart(myCart)
+  }
+
+  const clearCart = (_) => {
+    setCart({});
+    saveCart({});
+  }
+
+  const removeFromCart = (itemCode, qty) => {
+    const myCart = cart;
+    if (itemCode in myCart) {
+      myCart[itemCode].qty -= qty;
+    }
+    if (myCart[itemCode].qty <= 0) {
+      delete myCart[itemCode]
+    }
+    setCart(myCart);
+    saveCart(myCart);
+  }
+
   return (<>
     <Head>
       <title>Vastram - Ethentic Wears</title>
       <meta name="description" content="Vastram, clothes, ethentic, tradition clothes" />
       <link rel="shortcut icon" href={'public/logo.png'} type="image/x-icon" />
-      <meta name="viewport" content="width=device-width , initial-scale=1.0 , minimum-scale=1.0"/>
+      <meta name="viewport" content="width=device-width , initial-scale=1.0 , minimum-scale=1.0" />
     </Head>
-    <Navbar />
+    <Navbar addInCart={addInCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} cart={cart}/>
     <main className="flex min-h-screen flex-col items-center justify-between px-24 py-5 ">
-      <Component {...pageProps} />
+      <Component {...pageProps} addInCart={addInCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} cart={cart}/>
     </main>
     <Footer />
   </>
