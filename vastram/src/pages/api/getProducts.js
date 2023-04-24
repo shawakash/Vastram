c06ec6830763903b0c222c7067df8af478aba1e6
Akash Shaw <shawakash2003@gmail.com>
2023-04-24 13:39:02 +0530

    Commiting With Bug

--- vastram/src/pages/api/getProducts.js
+++ vastram/src/pages/api/getProducts.js
@@ -5,10 +5,33 @@ import Product from "../../../models/Product";
 const handler = async (req, res) => {
     try {
         const products = await Product.find();
-        return res.status(200).send(wrapResponse.success(200, products));
+        let tshirts = {};
 
-    } catch (error) {
-        return res.status(500).send(wrapResponse.error(500, 'Internal Server Error'));
+        for (let item of products) {
+            if (item.title in tshirts) {
+                if (!tshirts[item.title].color.includes(item.color) && availqty > 0) {
+                    tshirts[item.title].color.push(item.color);
+                }
+                if (!tshirts[item.title].size.includes(item.size) && availqty > 0) {
+                    tshirts[item.title].size.push(item.size);
+                }
+
+            } else {
+                tshirts[item.title] = JSON.parse(JSON.stringify(item));
+
+                if (item.availqty > 0) {
+                    tshirts[item.title].color = [item.color];
+                    tshirts[item.title].size = [item.size];
+                }
+
+            }
+        }
+
+
+        return res.status(200).send(wrapResponse.success(200, tshirts));
+
+    } catch (e) {
+        return res.status(500).send(wrapResponse.error(500, e.message));
     }
 }
 
